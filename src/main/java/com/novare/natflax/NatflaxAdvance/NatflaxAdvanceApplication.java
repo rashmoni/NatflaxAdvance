82505192f85309947f8e321c109a6550886f5295
rashmoni <rashudey9851@gmail.com>
2023-07-04 21:40:58 +0200

    first commit

---
+++ src/main/java/com/novare/natflax/NatflaxAdvance/NatflaxAdvanceApplication.java
@@ -0,0 +1,13 @@
+package com.novare.natflax.NatflaxAdvance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatflaxAdvanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatflaxAdvanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novare/natflax/NatflaxAdvance/NatflaxAdvanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novare.natflax.NatflaxAdvance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatflaxAdvanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
