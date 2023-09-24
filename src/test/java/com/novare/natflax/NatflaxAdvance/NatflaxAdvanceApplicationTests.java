e2d249c25ead9609317a52d66bc114d1e18d724e
rashmoni <rashudey9851@gmail.com>
2023-09-24 10:17:40 +0200

    Fixed test..

--- src/test/java/com/novare/natflax/NatflaxAdvance/NatflaxAdvanceApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.novare.natflax.NatflaxAdvance;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class NatflaxAdvanceApplicationTests {
-
-}
