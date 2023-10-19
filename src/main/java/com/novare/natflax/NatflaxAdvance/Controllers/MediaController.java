b03c9bf0ce6950bd41a8d3a7d4c1c7aa0da2f63d
rashmoni <rashudey9851@gmail.com>
2023-10-19 18:08:33 +0200

    Update....

--- src/main/java/com/novare/natflax/NatflaxAdvance/Controllers/MediaController.java
+++ src/main/java/com/novare/natflax/NatflaxAdvance/Controllers/MediaController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/v1/media/")
 @Log4j2
 public class MediaController {
